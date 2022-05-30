0d805111a83367cff0517c9b86628554cd34f9bc
Emilio <emiliothomasking@gmail.com>
2022-05-30 13:00:00 -0700

    add ability to sumbit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,14 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
     // package up data as an object
     var taskDataObj = {
     name: taskNameInput,
